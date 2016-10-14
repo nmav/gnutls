d6ee924ca38dc67978e85143c3a914921f4a5075
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-10-14 14:16:51 +0200

    rng: split initialization in preinit and init
    This makes gnutls to initialize its random generator on the
    first call to gnutls_rnd(). That prevents blocking due to
    getrandom() on a constructor; that change allows to use gnutls-linked
    applications even in early boot in systems where getrandom() blocks
    waiting for entropy.

--- lib/random.h
+++ lib/random.h
@@ -30,34 +30,17 @@ extern int crypto_rnd_prio;
 extern void *gnutls_rnd_ctx;
 extern gnutls_crypto_rnd_st _gnutls_rnd_ops;
 
-inline static int
-_gnutls_rnd(gnutls_rnd_level_t level, void *data, size_t len)
-{
-	if (len > 0) {
-		return _gnutls_rnd_ops.rnd(gnutls_rnd_ctx, level, data,
-					   len);
-	}
-	return 0;
-}
-
-inline static void _gnutls_rnd_refresh(void)
-{
-	_gnutls_rnd_ops.rnd_refresh(gnutls_rnd_ctx);
-}
+#define _gnutls_rnd gnutls_rnd
+#define _gnutls_rnd_refresh gnutls_rnd_refresh
 
 void _gnutls_rnd_deinit(void);
-int _gnutls_rnd_init(void);
+int _gnutls_rnd_preinit(void);
+
+#include "nettle/rnd-common.h"
 
 inline static int _gnutls_rnd_check(void)
 {
-	if (_gnutls_rnd_ops.check)
-		return _gnutls_rnd_ops.check(gnutls_rnd_ctx);
-	else
-		return 0;
+	return _rnd_system_entropy_check();
 }
-
-#ifndef _WIN32
-extern int _gnutls_urandom_fd;
-#endif
 
 #endif
